7dd9e7a4175c9bb514a2c5b92279fcbc9f1f1b45
Erdil Sualp BAYRAM <the.stylered@gmail.com>
2020-07-28 18:36:35 +0300

    AOD, Otomatik Parlaklık, Batarya değeri fixlendi ve DT2W vendor patch içine alındı.

--- Vendor Patch/install/bin/installer.sh
+++ Vendor Patch/install/bin/installer.sh
@@ -1,10 +1,14 @@
 #!/sbin/sh
 
 cp -rf /tmp/install/bin/vendor/etc/ /vendor/
+rm -rf /vendor/overlay/
+cp -rf /tmp/install/bin/vendor/overlay/ /vendor/
+
 
 # permissions
 
 chmod 644 /vendor/etc/audio/audio_policy_configuration.xml
 chmod 644 /vendor/etc/fstab.qcom
 chmod 644 /vendor/etc/media_profiles_V1_0.xml
-
+chmod 644 /vendor/overlay/framework-res_trzpro.apk
+chmod 755 /vendor/etc/init/hw/init.qcom.rc
